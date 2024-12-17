95bf3cad57c0d90be8f9573079069a6b1309325a
Tatsuya Nakagomi <sanadahyun0918gromin28@gmail.com>
2024-12-17 13:10:33 +1100

    Fixing

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { API_AUTH_PREFIX, authRoutes, DEFAULT_LOGIN_REDIRECT, DEFAULT_REDIRECT, privateRoutes, publicRoutes, setAuthRoutes } from "./routes";
-import { NextResponse } from "next/server";
 
 const {auth: middleware} = NextAuth(authConfig);
 
@@ -43,31 +42,6 @@ export default middleware((req) => {
     }
     return;
 });
-
-export const apiMiddleware = (req: Request) => {
-    const allowedOrigins = ['https://household-management-app.vercel.app', 'http://localhost:3000']; // Adjust based on your frontend URLs
-    const origin = req.headers.get('origin');
-  
-    // Check if the origin is allowed
-    if (allowedOrigins.includes(origin as string)) {
-      const response = NextResponse.next(); // Continue processing the request
-  
-      // Set CORS headers
-      response.headers.set('Access-Control-Allow-Origin', origin as string);
-      response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      
-      // Handle preflight requests (OPTIONS method)
-      if (req.method === 'OPTIONS') {
-        // Respond with status 200 and end the request
-        return response;
-      }
-  
-      return response;
-    }
-  
-    return NextResponse.next();
-  };
 
 // Do not miss to correctly handle the above
 export const config = {
