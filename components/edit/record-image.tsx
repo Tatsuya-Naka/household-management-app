213616d365b151c637c276586f255496f8ec4646
Tatsuya Nakagomi <sanadahyun0918gromin28@gmail.com>
2025-01-16 18:44:37 +1100

    FIx UI

--- components/edit/record-image.tsx
+++ components/edit/record-image.tsx
@@ -24,41 +24,48 @@ export default function EditRecordImage({ object }: EditRecordImageProps) {
     }
 
     return (
-        <label className={`${image && "border-2 border-solid border-gray-500/30"} relative overflow-hidden mb-3 h-[240px] flex items-start justify-center bg-gradient-to-r from-sky-300 via-violet-300 to-pink-100 w-full rounded-lg cursor-pointer hover:opacity-80`}>
-            {image ?
-                <>
-                    <div className="absolute inset-0">
+        <div className="w-full h-[240px] relative mb-3">
+            <label className={`${image && "border-2 border-solid border-gray-500/30"} relative overflow-hidden h-[240px] flex items-start justify-center bg-gradient-to-r from-sky-300 via-violet-300 to-pink-100 w-full rounded-lg cursor-pointer hover:opacity-80`}>
+                {image ?
+                    <>
+                        <div className="absolute inset-0">
 
-                        <Image
-                            src={image}
-                            alt={"Record Image"}
-                            fill
-                            className="object-cover rounded-lg"
-                        />
+                            <Image
+                                src={image}
+                                alt={"Record Image"}
+                                fill
+                                className="object-cover rounded-lg"
+                            />
+                        </div>
+
+                    </>
+                    :
+                    <div className="w-full flex items-center justify-center h-full">
+                        <IoReceiptOutline size={48} className="fill-black mr-3" />
+                        <span>Upload image</span>
                     </div>
+                }
+                <input
+                    type="file"
+                    hidden
+                    name="object"
+                    onChange={handleImageFile}
+                // disabled={isSubmitting}
+                />
+            </label>
 
-                    {/* handleRemove */}
+            {/* handleRemove */}
+            {image &&
+                <div className="absolute right-2 top-2 z-[999]">
                     <button
-                        className="absolute right-2 top-2 z-10 p-1.5 rounded-md hover:bg-white/50 transition duration-100 ease-in cursor-pointer"
+                        className=" p-1.5 rounded-md hover:bg-white/50 transition duration-100 ease-in cursor-pointer"
                         type="button"
                         onClick={handleRemoveImage}
                     >
                         <IoIosRemove size={32} className="fill-slate-800" />
                     </button>
-                </>
-                :
-                <div className="w-full flex items-center justify-center h-full">
-                    <IoReceiptOutline size={48} className="fill-black mr-3" />
-                    <span>Upload image</span>
                 </div>
             }
-            <input
-                type="file"
-                hidden
-                name="object"
-                onChange={handleImageFile}
-            // disabled={isSubmitting}
-            />
-        </label>
+        </div>
     )
 }
