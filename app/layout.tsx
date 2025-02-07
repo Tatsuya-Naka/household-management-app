170d864c99315e6266871451d4aaa351135b0cd6
Tatsuya Nakagomi <sanadahyun0918gromin28@gmail.com>
2025-02-07 12:24:15 +1100

    add image src into main branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "HMB - Household Management App",
   description: "Manage your money with your everyday life",
+  openGraph: {
+    images: ['https://household-management-bucket.s3.ap-southeast-2.amazonaws.com/icon/cm4p59ynp0000z4ngvd05ehpo']
+  }
 };
 
 export default function RootLayout({
