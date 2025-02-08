02680a05778d82a280ed389460f28a3d85010908
Tatsuya Nakagomi <sanadahyun0918gromin28@gmail.com>
2025-02-08 12:09:41 +1100

    generateMetadata const not function

--- app/edit/records/[recordId]/page.tsx
+++ app/edit/records/[recordId]/page.tsx
@@ -1,4 +1,6 @@
 import EditRecordsComponent from "@/components/edit/records/edit-records";
+import { db } from "@/lib/db";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 interface EditRecordsPageProps {
@@ -7,12 +9,36 @@ interface EditRecordsPageProps {
   }>
 }
 
-export default async function EditRecordsPage({params}: EditRecordsPageProps) {
-    const {recordId} = await params;
+export const generateMetadata = async ({ params }: EditRecordsPageProps): Promise<Metadata> => {
+  const { recordId } = await params;
+  const data = await db.record.findUnique({ where: { id: recordId } });
+  const openGraph = {
+    title: "HEB - healthy financial management app",
+    description: `testing - ${data?.comment}`,
+    url: `https://household-management-app.vercel.app/edit/records/${recordId}`,
+    images: [{url: `${data?.object ? data.object : "https://household-management-bucket.s3.ap-southeast-2.amazonaws.com/icon/cm4p59ynp0000z4ngvd05ehpo"}`, width: 800, height: 600, alt: "image"}],
+    type: "website"
+  }
 
-    return (
-        <Suspense>
-            <EditRecordsComponent recordId={recordId} />
-        </Suspense>
-    ) 
+  return {
+    title: "HEB application | Tatsuya",
+    description: `dev - ${data?.comment}`,
+    openGraph: openGraph,
+    twitter: {
+      card: "summary_large_image",
+      title: `HEB`,
+      description: `twitter - ${data?.typeId}`,
+      images: [`${data?.object} || https://household-management-bucket.s3.ap-southeast-2.amazonaws.com/icon/cm4p59ynp0000z4ngvd05ehpo`]
+    }
+  }
+}
+
+export default async function EditRecordsPage({ params }: EditRecordsPageProps) {
+  const { recordId } = await params;
+
+  return (
+    <Suspense>
+      <EditRecordsComponent recordId={recordId} />
+    </Suspense>
+  )
 }
