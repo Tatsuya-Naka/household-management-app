d8a2eace7c2f471fc8b08faad54ea2105593f3ca
Tatsuya Nakagomi <sanadahyun0918gromin28@gmail.com>
2025-02-14 10:36:31 +1100

    add generateMetadata into dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,26 +2,26 @@ import { auth } from '@/auth';
 import RecordsDialog from '@/components/header/records-dialog';
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const session = await auth();
-    return (
-        <div className="max-w-full ">
-            <header className="w-full px-5 py-3 h-[180px] bg-gradient-to-b from-sky-400 via-teal-200 to-emerald-400">
-                <div className='max-w-[1560px] relative z-[80] mx-auto h-full'>
-                    <RecordsDialog id={session?.user.id} name={session?.user.name} image={session?.user.image} page="dashboard" />
+  const session = await auth();
+  return (
+    <div className="max-w-full ">
+      <header className="w-full px-5 py-3 h-[180px] bg-gradient-to-b from-sky-400 via-teal-200 to-emerald-400">
+        <div className='max-w-[1560px] relative z-[80] mx-auto h-full'>
+          <RecordsDialog id={session?.user.id} name={session?.user.name} image={session?.user.image} page="dashboard" />
 
-                    {/* If I click this, z-index should be z-10 */}
-                    <button
-                        type="button"
-                        className="absolute z-0 bottom-2 right-2 left-auto px-5 py-1.5 rounded-md bg-black hover:bg-black/50 text-white text-base"
-                    >
-                        Quick
-                    </button>
-                </div>
-            </header>
-
-            <main className='max-w-[1560px]  pr-[81px] min-h-[calc(100%-180px)]'>
-                {children}
-            </main>
+          {/* If I click this, z-index should be z-10 */}
+          <button
+            type="button"
+            className="absolute z-0 bottom-2 right-2 left-auto px-5 py-1.5 rounded-md bg-black hover:bg-black/50 text-white text-base"
+          >
+            Quick
+          </button>
         </div>
-    )
+      </header>
+
+      <main className='max-w-[1560px]  pr-[81px] min-h-[calc(100%-180px)]'>
+        {children}
+      </main>
+    </div>
+  )
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,43 @@
 import DashboardContainer from "@/components/dashboard/dashboard-container";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'HAB - Household Account Book Online',
+  description: 'Manage your household financial status with robust security',
+  openGraph: {
+    title: 'HAB - Household Account Book Online',
+    description: 'Manage your household financial status with robust security',
+    url: 'https://household-management-app.vercel.app/',
+    images: [
+      {
+        // url: '/hima_pajamas.png',
+        url: 'https://household-management-bucket-temp.s3.ap-southeast-2.amazonaws.com/new-record/cm4p59ynp0000z4ngvd05ehpo/44d0563a-01b1-4d72-ab15-4cb91d0036b5',
+        width: 800,
+        height: 600,
+        alt: 'image himachan'
+      }
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'HAB - Household Account Book Online',
+    description: 'Manage your household financial status with robust security',
+    // images: ['/hima_pajamas.png']
+    images: ['https://household-management-bucket-temp.s3.ap-southeast-2.amazonaws.com/new-record/cm4p59ynp0000z4ngvd05ehpo/44d0563a-01b1-4d72-ab15-4cb91d0036b5']
+  }
+  }
+}
+
 export default async function DashboardPage() {
-    return (
-        <div className="">
-            {/* Dashboard Page */}
-            <Suspense fallback={"...Loading"}>
-                <DashboardContainer />
-            </Suspense>
-        </div>
-    )
+  return (
+    <div className="">
+      {/* Dashboard Page */}
+      <Suspense fallback={"...Loading"}>
+        <DashboardContainer />
+      </Suspense>
+    </div>
+  )
 }
